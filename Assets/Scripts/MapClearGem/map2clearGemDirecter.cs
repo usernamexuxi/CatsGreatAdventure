f9eef8a70cafea9d26ebb8dc32252c8b63b9032a
usernamexuxi <116962624+usernamexuxi@users.noreply.github.com>
2022-11-04 20:55:42 +0900

    Add files via upload

---
+++ Assets/Scripts/MapClearGem/map2clearGemDirecter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class map2clearGemDirecter : MonoBehaviour
+{
+    public float time;
+    int num = 0;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        num = (int)time;
+
+        if (num > 0.4)
+        {
+            SceneManager.LoadScene("map2clearAniScene");
+        }
+    }
+}
